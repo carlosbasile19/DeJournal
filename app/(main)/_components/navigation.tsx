1306f25517636d94d89b4ae7329f770cdcfd9c83
Carlitos <basilecarlos12@gmail.com>
2024-08-23 21:30:12 +0200

    Navbar completed

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,7 +3,7 @@
 import { cn } from "@/lib/utils";
 import { useMediaQuery} from "usehooks-ts";
 import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { UserItem } from "./user-item";
 import { useMutation, useQuery } from "convex/react";
@@ -19,11 +19,13 @@ import {
 import { TrashBox } from "./trash-box";
 import { useSearch } from "@/hooks/use-search";
 import { useSettings } from "@/hooks/use-settings";
+import Navbar from "./navbar";
 
 const Navigation = () => {
    
     const search = useSearch();
     const settings = useSettings();
+    const params = useParams();
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
     
@@ -199,9 +201,19 @@ const Navigation = () => {
           isMobile && "left-0 w-full"
         )}
         >
-            <nav className="bg-transparent px-3 py-2 w-full">
-                {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground"/>}
-            </nav>
+            {!!params.documentId ? (
+              <Navbar
+                 isCollapsed={isCollapsed}
+                 onResetWidth={resetWidth}
+              />
+            ) : (
+                  <nav className="bg-transparent px-3 py-2 w-full">
+                    {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground"/>}
+                  </nav>
+                )
+            
+            }
+            
         </div>
 
        </>
