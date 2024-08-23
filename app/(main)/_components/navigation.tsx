426302744a6e65753149fb3bd6279067504a68b7
Carlitos <basilecarlos12@gmail.com>
2024-08-23 10:29:44 +0200

    Trashbin Completed

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { useMediaQuery} from "usehooks-ts";
-import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { UserItem } from "./user-item";
@@ -11,6 +11,12 @@ import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import {toast } from "sonner";
 import { DocumentList } from "./document-list";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { TrashBox } from "./trash-box";
 
 const Navigation = () => {
    
@@ -156,7 +162,24 @@ const Navigation = () => {
         </div>
         <div className="mt-4">
             <DocumentList />
+            <Item
+            onClick={handleCreate}
+            icon={Plus}
+            label="Add a page"
+          />
+            
         </div>
+        <Popover>
+            <PopoverTrigger className="w-full mt-4">
+              <Item label="Trash" icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent
+              className="p-0 w-72"
+              side={isMobile ? "bottom" : "right"}
+            >
+              <TrashBox />
+            </PopoverContent>
+          </Popover>
         <div 
           onMouseDown={handleMouseDown}
           onClick={resetWidth}
